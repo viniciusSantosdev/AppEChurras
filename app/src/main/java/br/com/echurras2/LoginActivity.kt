0b1d4f22ed4230251920cb60ea274024aa8e3882
Vincius Santos <santos.viniciusmt@gmail.com>
2020-03-03 21:15:28 -0300

    Arrumei alguns erros

--- app/src/main/java/br/com/echurras2/LoginActivity.kt
+++ app/src/main/java/br/com/echurras2/LoginActivity.kt
@@ -1,12 +1,30 @@
 package br.com.echurras2
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Build
+import android.view.Window
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import br.com.echurras2.R
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+            window.setStatusBarColorTo(R.color.colorPrimary)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun Window.setStatusBarColorTo(color:Int){
+        this.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        this.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        this.statusBarColor= ContextCompat.getColor(baseContext, color)
     }
 }
--- app/src/main/java/br/com/echurras2/MainActivity.kt
+++ app/src/main/java/br/com/echurras2/MainActivity.kt
@@ -2,17 +2,25 @@ package br.com.echurras2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
+import android.content.Intent
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN) //This code remove the action bar
         setContentView(R.layout.activity_main)
+        changeToLogin()
+    }
+    fun changeToLogin(){
+        val intent = Intent(this, LoginActivity::class.java)
 
-
+        Handler().postDelayed({
+            intent.change()
+        }, 2000)
+    }
+    fun Intent.change(){
+        startActivity(this)
+        finish()
     }
 }
