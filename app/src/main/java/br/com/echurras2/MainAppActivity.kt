873339dad37807c489902989bec425dd4e72890b
Vinicius Santos] <santos.viniciusmt@gmail.com>
2020-03-09 21:40:44 -0300

    Termina os campos de cada tela

--- app/src/main/java/br/com/echurras2/MainAppActivity.kt
+++ app/src/main/java/br/com/echurras2/MainAppActivity.kt
@@ -1,12 +1,19 @@
 package br.com.echurras2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainAppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_app)
+    }
+
+    fun listarItens(v: View){
+        var intent = Intent(this, ListaItensActivity::class.java)
+        startActivity(intent)
     }
 }
