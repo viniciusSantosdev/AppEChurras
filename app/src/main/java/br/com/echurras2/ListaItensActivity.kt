bc35edef7c2e32e5432713882e2972ad44ee0f49
Vinicius Santos <santos.viniciusmt@gmail.com>
2020-03-12 21:06:22 -0300

    Termine a transicao de tela

--- app/src/main/java/br/com/echurras2/ListaItensActivity.kt
+++ app/src/main/java/br/com/echurras2/ListaItensActivity.kt
@@ -1,12 +1,19 @@
 package br.com.echurras2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ListaItensActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_itens)
+    }
+
+    fun salvarLista(v: View){
+        var intent = Intent(this, MainAppActivity::class.java)
+        startActivity(intent)
     }
 }
